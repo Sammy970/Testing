46a34237451ff5f09b68b4cb757516e5938e9896
Sammy970 <70690987+Sammy970@users.noreply.github.com>
2020-12-10 14:24:43 +0530

    Create CompressPEX.sh

---
+++ CompressPEX.sh
@@ -0,0 +1,18 @@
+cd
+wget https://github.com/git-lfs/git-lfs/releases/download/v2.9.0/git-lfs-linux-amd64-v2.9.0.tar.gz
+tar -xf git-lfs-linux-amd64-v2.9.0.tar.gz
+./install.sh
+cd
+git clone https://github.com/Sammy970/sourcepex.git sam
+cd 
+cd bin/build
+tar cvzf source.tar.gz pex
+tar cvzf source.tar.gz pex
+tar cvzf source.tar.gz pex
+cd
+mv bin/build/source.tar.gz ~
+mv source.tar.gz sam
+cd sam
+git lfs install
+git lfs track ".tar.gz"
+git lfs track "tar.gz"
