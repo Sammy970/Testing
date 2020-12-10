4ed97957da4dd13a57c6f8a7dc7dc68f0c35249e
Sammy970 <70690987+Sammy970@users.noreply.github.com>
2020-12-10 14:34:33 +0530

    Update CompressPEX.sh

--- CompressPEX.sh
+++ CompressPEX.sh
@@ -5,14 +5,14 @@ tar -xf git-lfs-linux-amd64-v2.9.0.tar.gz
 cd
 git clone https://github.com/Sammy970/sourcepex.git sam
 cd 
-cd bin/build
+cd bin/build/
 tar cvzf source1.tar.gz pex
 tar cvzf source2.tar.gz source1.tar.gz
 tar cvzf source3.tar.gz source2.tar.gz
 cd
 mv bin/build/source3.tar.gz ~
-mv source.tar.gz sam
-cd sam
+mv source.tar.gz sam/
+cd sam/
 git lfs install
 git lfs track ".tar.gz"
 git lfs track "tar.gz"
